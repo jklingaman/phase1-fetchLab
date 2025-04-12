91a68060f995821555ce9f598b8d75cc815a8699
Jackson Klingaman <jackson.klingaman22@gmail.com>
2025-04-11 19:27:49 -0500

    passes all tests

--- index.js
+++ index.js
@@ -1,6 +1,21 @@
+
 function fetchBooks() {
   // To pass the tests, don't forget to return your fetch!
-  
+  fetch("https://anapioficeandfire.com/api/books")
+  .then((resp) => resp.json())
+  .then((json) => {
+    const fifthBook = json[4];
+    renderBooks([fifthBook])
+
+    const totalPages = countPages(json);
+    renderTotalPages(totalPages)
+  });
+  fetch("https://anapioficeandfire.com/api/characters/1031")
+  .then((resp) => resp.json())
+  .then((json) => {
+    const characterName = json.name;
+    renderName(characterName)
+  });
 }
 
 function renderBooks(books) {
@@ -12,6 +27,27 @@ function renderBooks(books) {
   });
 }
 
+function renderName(name) {
+  const main = document.querySelector('main');
+  const h2 = document.createElement('h2');
+  h2.innerHTML = name;
+  main.appendChild(h2);
+}
+
+function countPages(books) {
+  return books.reduce((total, book) => {
+    return total + book.numberOfPages;
+  }, 0);
+}
+
+function renderTotalPages(totalPages) {
+  const main = document.querySelector('main');
+  const h2 = document.createElement('h2');
+  h2.innerHTML = `Total page count: ${totalPages}`;
+  main.appendChild(h2);
+}
+
 document.addEventListener('DOMContentLoaded', function() {
   fetchBooks();
 });
+
